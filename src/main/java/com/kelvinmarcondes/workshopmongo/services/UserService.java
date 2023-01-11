49165b210c3e9dc2fc71d993363d3477ecd236c6
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-10 23:06:44 -0300

    Obtendo um usuário por id

--- src/main/java/com/kelvinmarcondes/workshopmongo/services/UserService.java
+++ src/main/java/com/kelvinmarcondes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.kelvinmarcondes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kelvinmarcondes.workshopmongo.domain.User;
 import com.kelvinmarcondes.workshopmongo.repository.UserRepository;
+import com.kelvinmarcondes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,8 +18,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
-		
 	}
+	
+	public User findById (String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto Não encontrado"));
+	}
+	
+	
 	
 	
 
