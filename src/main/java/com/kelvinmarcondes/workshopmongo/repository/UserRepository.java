9eacf46b94a6388529b380ae075dd364c5ce4753
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-10 21:51:36 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/kelvinmarcondes/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kelvinmarcondes.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kelvinmarcondes.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
